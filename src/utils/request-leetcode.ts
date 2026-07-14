0a372306a884495bf1e8ad4a2063f8ff75401837
Alina <wuangelina40@gmail.com>
2026-07-14 00:07:01 -0700

    chore: add comment to LeetCode GraphQL documentation link in request-leetcode.ts

--- src/utils/request-leetcode.ts
+++ src/utils/request-leetcode.ts
@@ -24,8 +24,11 @@ export default function requestLeetCode(data: any): AxiosPromise<any> {
     });
 }
 
-/* Output structure for reference:
-   ===============================
+/*
+Unofficial LeetCode GraphQL API Documentation: https://github.com/akarsh1995/leetcode-graphql-queries
+
+Output structure for reference (non-exhaustive):
+================================================
 PS C:\> (Invoke-RestMethod -Uri "https://leetcode.com/graphql" -Method Post -Headers @{"Content-Type"="application/json"} -Body '{"query":"query($u:String!){matchedUser(username:$u){username profile{realName userAvatar aboutMe reputation ranking starRating company school countryName websites skillTags} submitStatsGlobal{acSubmissionNum{difficulty count submissions}} languageProblemCount{languageName problemsSolved} badges{id displayName icon} activeBadge{displayName icon}} userContestRanking(username:$u){attendedContestsCount rating globalRanking totalParticipants topPercentage badge{name}} userContestRankingHistory(username:$u){attended rating ranking problemsSolved totalProblems finishTimeInSeconds contest{title startTime}} recentSubmissionList(username:$u){title titleSlug statusDisplay lang timestamp}}","variables":{"u":"angiebaby"}}') | ConvertTo-Json -Depth 100
 {
     "data":  {
